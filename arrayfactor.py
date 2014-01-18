19d765357758bb6dcddb0f107e5aecebc56eaec8
lonAlpha <jinyao@server.fake>
2014-01-19 00:49:01 +0800

    Nanbo Jin's result verified.

--- arrayfactor.py
+++ arrayfactor.py
@@ -3,7 +3,8 @@
 Compute array factor of 1d and 2d antenna array.
 """
 
-from numpy import linspace, abs, sum, pi, sin, exp, log10, max, zeros
+from numpy import linspace, abs, sum, pi, sin, exp, log10, max, zeros, array,concatenate
+import pylab
 
 #1D array factor
 #Unit of AF: dB
@@ -29,7 +30,23 @@ def OneDimSLL(AF):
     # return max sidelobe level
     return max(a1)
 
-if __name__=='__main__':
+def NanbojinTest():
+    arrayData = [0.236, 0.631, 1.088, 1.515, 2.055, 2.542, 3.173, 3.953]
+    arrayData.append(4.75)
+    arrayData = array(arrayData)
+    pos = concatenate((-1*(arrayData[::-1]),arrayData))
+    num = len(pos)
+    amp = 1.0
+    phase = 0.0
+    AF = zeros(360)
+    for i in xrange(360):
+        AF[i] = OneDimAF(num, pos, amp, phase,i/180.*pi) 
+    AF = AF - max(AF)
+    print 'SLL of Nanbo Jin''s array is ', OneDimSLL(AF)
+#    print 'SLL is ', OneDimSLL(AF)
+
+
+def simple_test():
     import pylab
     num = 20
     # distance between element is 1/2*wavelength
@@ -43,3 +60,6 @@ if __name__=='__main__':
     pylab.plot(range(360), AF)
     pylab.show()
     print 'SLL of', num, 'elements array is', OneDimSLL(AF), 'dB'
+
+if __name__=='__main__':
+    NanbojinTest()
